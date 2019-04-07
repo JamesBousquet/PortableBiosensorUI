fbee838f5818430f06a2553e27f98b3e476b02d0
Bousquet <james_bousquet@baylor.edu>
2019-04-07 16:00:13 -0500

    update makefile for proj struct

--- MakeFileProjStruct.py
+++ MakeFileProjStruct.py
@@ -1,8 +1,8 @@
 import os
 # run this on new machines to create the file structure needed for main.py and processing.py
 
-os.system("sudo touch processedData.txt")
-os.system("sudo touch rawData.txt")
-os.system("sudo touch aoi.txt")
-os.system("sudo touch denominator.txt")
-os.system("sudo touch numerator.txt")
+os.system("sudo touch Data/processedData.txt")
+os.system("sudo touch Data/rawData.txt")
+os.system("sudo touch Equations/aoi.txt")
+os.system("sudo touch Equations/denominator.txt")
+os.system("sudo touch Equations/numerator.txt")
