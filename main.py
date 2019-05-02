fe16078c8a2ca81dc9048c1a01edf78488d3c626
James Bousquet <BOUSQUET@ECS.BAYLOR.EDU>
2019-05-01 22:34:35 -0500

    s

--- main.py
+++ main.py
@@ -169,10 +169,9 @@ class PortableBiosensorUI(tk.Tk):
             
         menu = tk.Menu(self,bg = "blue",fg=TEXT_COLOR)
         self.config(menu=menu)
-		menu.add_command(label="Return", command = self.backToStartPage)
         self.show_frame(StartPage)
         menu.add_command(label="Start Page", command = self.backToStartPage)
-		menu.add_command(label="Return", command = self.backToStartPage)
+	menu.add_command(label="Return", command = self.backToStartPage)
         self.show_frame(StartPage)
     def show_frame(self,cont):
         frame = self.frames[cont]
